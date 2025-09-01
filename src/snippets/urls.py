b69d086638378565d1fbd5bf8511ea6e38b0eb0b
Devansh <devanshojha0505@gmail.com>
2025-08-31 22:59:57 -0700

    intial

---
+++ src/snippets/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from snippets import views
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),  
+    path('api-auth/', include('rest_framework.urls')),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
